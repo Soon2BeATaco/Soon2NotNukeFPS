f8dd9b36870ddd2cea39e2bf6361d3abc5c05a18
oblongboot <81798709+oblongboot@users.noreply.github.com>
2025-07-03 17:50:34 +0100

    getUUID() function, GeneralUtils file, currently only contains mc object but oh well

--- src/main/kotlin/com/github/soon2beataco/soon2notnukefps/utils/APIUtils.kt
+++ src/main/kotlin/com/github/soon2beataco/soon2notnukefps/utils/APIUtils.kt
@@ -1,10 +1,12 @@
 package com.github.soon2beataco.soon2notnukefps.utils
 
 import com.github.soon2beataco.soon2notnukefps.utils.ChatUtils.modMessage
+import com.github.soon2beataco.soon2notnukefps.utils.GeneralUtils.mc
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.net.HttpURLConnection
 import java.net.URL
+import org.json.JSONObject
 
 object APIUtils {
     private const val baseUrl = "https://tacoaddons.tacoskyblockapi.workers.dev/"
@@ -16,15 +18,15 @@ object APIUtils {
     * @returns the response body as a String or null if the request fails.
     */
 
-    suspend fun request(endpoint: String = ""): String? {
+    suspend fun request(endpoint: String = "", useBaseURL: Boolean = true): String? {
         return withContext(Dispatchers.IO) {
             var connection: HttpURLConnection? = null
             try {
-                val url = URL(baseUrl + endpoint)
+                val url = if (useBaseURL) URL(baseUrl + endpoint) else URL(endpoint) // untested but prolly works ig
                 connection = url.openConnection() as HttpURLConnection
                 connection.requestMethod = "GET"
                 connection.setRequestProperty("Accept", "application/json")
-                connection.setRequestProperty("User-Agent", "Mozilla/5.0")
+                connection.setRequestProperty("User-Agent", "Mozilla/5.0") // todo: stop this shit from being suspend so i dont have to use fucking coroutines on every instance
                 connection.connectTimeout = 5000
                 connection.readTimeout = 5000
 
@@ -43,4 +45,9 @@ object APIUtils {
             }
         }
     }
+    suspend fun getUUID(ign: String = mc.thePlayer?.name ?: return): String? {
+        val json = request("https://api.mojang.com/users/profiles/minecraft/$ign", false)
+        return json?.let {
+            JSONObject(it).getString("id")
+        }
 }
---
+++ src/main/kotlin/com/github/soon2beataco/soon2notnukefps/utils/GeneralUtils.kt
@@ -0,0 +1,8 @@
+package com.github.soon2beataco.soon2notnukefps.utils
+
+import com.github.soon2beataco.soon2notnukefps.utils.ChatUtils.modMessage
+import net.minecraft.client.Minecraft
+
+object GeneralUtils {
+    public val mc: Minecraft = Minecraft.getMinecraft()
+}
